43c95de44ea58eab12dbcebc2a65ffd67c90de1b
Pablo Catalina <pcatalina@alienvault.com>
2010-09-07 12:00:19 +0200

    Update LinuxDHCP plugin to parse DHCP3-Server logs

--- os-sim/db/plugins/linuxdhcp.sql
+++ os-sim/db/plugins/linuxdhcp.sql
@@ -7,13 +7,17 @@ DELETE FROM plugin_sid WHERE plugin_id=1607;
 
 INSERT INTO plugin (id, type, name, description) VALUES (1607, 1, 'linuxdhcp', 'Linux DHCP Service Activity');
 
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 1, NULL, NULL, 1, 1, 'DHCP Request');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 2, NULL, NULL, 1, 1, 'DHCP ACK');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 3, NULL, NULL, 1, 1, 'DHCP Offer');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 4, NULL, NULL, 1, 1, 'DHCP Inform');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 5, NULL, NULL, 1, 1, 'DHCP Release');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 6, NULL, NULL, 1, 1, 'DHCP Lease is duplicate');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 7, NULL, NULL, 1, 1, 'DHCP Relay Agent with Circuit');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 8, NULL, NULL, 1, 1, 'DHCP No free leases');
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, reliability, priority, name) VALUES (1607, 9, NULL, NULL, 1, 1, 'DHCP Discover');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 1, 1, 1, 'DHCP Request');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 2, 1, 1, 'DHCP ACK');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 3, 1, 1, 'DHCP Offer');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 4, 1, 1, 'DHCP Inform');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 5, 1, 1, 'DHCP Release');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 6, 1, 1, 'DHCP Lease is duplicate');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 7, 1, 1, 'DHCP Relay Agent with Circuit');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 8, 2, 2, 'DHCP No free leases');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 9, 1, 1, 'DHCP Discover');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 10, 1, 1, 'DHCP NAK');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 11, 1, 1, 'DHCP Decline');
+INSERT INTO plugin_sid (plugin_id, sid, reliability, priority, name) VALUES (1607, 12, 1, 1, 'DHCP Boot request');
+
 
