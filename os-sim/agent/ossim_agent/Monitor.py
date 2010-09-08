f238c8a9fc1e9589c8ab844f98165204b9fffd15
firnsy <firnsy@securixlive.com>
2010-09-08 19:42:53 +0930

    fixed: check for instance type when processing the monitor_response added: licensing information and initial cleanup of class structure (cherry picked from commit 8311aff657bf50b8e7da160dde3711846f6f6ba9)

--- os-sim/agent/ossim_agent/Monitor.py
+++ os-sim/agent/ossim_agent/Monitor.py
@@ -1,11 +1,47 @@
+#
+# License:
+#
+#    Copyright (c) 2003-2006 ossim.net
+#    Copyright (c) 2007-2010 AlienVault
+#    All rights reserved.
+#
+#    This package is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; version 2 dated June, 1991.
+#    You may not use, modify or distribute this program under any other version
+#    of the GNU General Public License.
+#
+#    This package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this package; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+#    MA  02110-1301  USA
+#
+#
+# On Debian GNU/Linux systems, the complete text of the GNU General
+# Public License can be found in `/usr/share/common-licenses/GPL-2'.
+#
+# Otherwise you can read it here: http://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+#
+# GLOBAL IMPORTS
+#
+import re, time
+
+#
+# LOCAL IMPORTS
+#
+from Config import Conf, Plugin, Aliases, CommandLineOptions
 from Logger import Logger
-logger = Logger.logger
 from Output import Output
 from Stats import Stats
-import Config
-from Config import Conf, Plugin, Aliases, CommandLineOptions
+logger = Logger.logger
 
-import re, time
 
 class Monitor:
 
@@ -215,7 +251,11 @@ class Monitor:
         regexp = self.regexps[rule_name]
         pattern = re.compile(regexp, re.IGNORECASE|re.MULTILINE)
         
-        match = pattern.search(monitor_response)
+        # TODO: monitor_response could possibly be a list
+        if isinstance(monitor_response, list):
+            match = pattern.search(monitor_response[0])
+        else:
+            match = pattern.search(monitor_response)
 
         if match is not None:
             groups = match.groups()
