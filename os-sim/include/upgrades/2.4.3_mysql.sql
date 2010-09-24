8c2480f85a6c9b92e0f69d97ea35b2852b9f0d6f
Juan Manuel Albarracin <jmalbarracin@alienvault.com>
2010-09-24 14:14:54 +0200

    Update mysql schema. Netflows reports added

--- os-sim/include/upgrades/2.4.3_mysql.sql
+++ os-sim/include/upgrades/2.4.3_mysql.sql
@@ -4,6 +4,9 @@ BEGIN;
 
 UPDATE custom_report_types SET inputs=CONCAT(inputs,";Source:source:select:OSS_ALPHA:EVENTSOURCE:") WHERE file='SIEM/List.php' AND name!="List" AND inputs not like '%EVENTSOURCE%';
 INSERT IGNORE INTO  `action_type` (`_type` , `descr`) VALUES ('ticket',  'generate ticket');
+REPLACE INTO `custom_report_types` (`id`, `name`, `type`, `file`, `inputs`, `sql`, `dr`) VALUES
+(515, 'NetFlows - Trafic Graphs', 'Network', 'Network/TraficGraphs.php', 'TCP:tcp:checkbox:OSS_NULLABLE.OSS_DIGIT:1;UDP:udp:checkbox:OSS_NULLABLE.OSS_DIGIT:1;ICMP:icmp:checkbox:OSS_NULLABLE.OSS_DIGIT:1;ANY:any:checkbox:OSS_NULLABLE.OSS_DIGIT:1', '', 1),
+(516, 'NetFlows - Trafic Details', 'Network', 'Network/NetFlows.php', 'Source:SOURCE:multiselect:OSS_ALPHA.OSS_COLON.OSS_SPACE.OSS_SCORE.OSS_DOT;Top Flows:top:text:OSS_DIGIT:20:500;Type List:Type_list:radiobuttons:OSS_ALPHA.OSS_COLON.OSS_SPACE.OSS_SCORE.OSS_DOT', '', 1);
 
 use snort;
 DROP PROCEDURE IF EXISTS addcol;
@@ -19,7 +22,7 @@ END;
 DELIMITER ';'
 CALL addcol();
 DROP PROCEDURE addcol;
-            
+
 use ossim;
 -- From now on, always add the date of the new releases to the .sql files
 UPDATE config SET value="2010-09-24" WHERE conf="last_update";
