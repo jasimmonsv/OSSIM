b11569e02b801295220026ee123ef65b48cfb824
firnsy <firnsy@securixlive.com>
2010-10-10 14:14:12 +1030

    changed: cosmetic alignment towards the PEP 8 and 257 standards.

--- os-sim/agent/ossim_agent/MonitorSession.py
+++ os-sim/agent/ossim_agent/MonitorSession.py
@@ -1,19 +1,64 @@
-from Monitor import Monitor
+#
+# License:
+#
+#    Copyright (c) 2003-2006 ossim.net
+#    Copyright (c) 2007-2010 AlienVault
+#    All rights reserved.
+#
+#    This package is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; version 2 dated June, 1991.
+#    You may not use, modify or distribute this program under any other version
+#    of the GNU General Public License.
+#
+#    This package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this package; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+#    MA  02110-1301  USA
+#
+#
+# On Debian GNU/Linux systems, the complete text of the GNU General
+# Public License can be found in `/usr/share/common-licenses/GPL-2'.
+#
+# Otherwise you can read it here: http://www.gnu.org/licenses/gpl-2.0.txt
+#
 
+#
+# GLOBAL IMPORTS
+#
+import re
+import sgmllib
+import socket
+import urllib
+
+#
+# LOCAL IMPORTS
+#
 from Logger import Logger
+from Monitor import Monitor
 from SessionParser import SessionParser
+
+#
+# GLOBAL VARIABLES
+#
 logger = Logger.logger
 
-import urllib, socket, sgmllib, re
+
 
 class MonitorSession(Monitor):
 
-    # connect to monitor
     def open(self):
+        """Connect to monitor."""
         return
 
-    # get data from monitor
+
     def get_data(self, rule_name):
+        """Get data from monitor."""
 
         data = ''
         location = self.plugin.get("config", "location")
@@ -24,9 +69,11 @@ class MonitorSession(Monitor):
         try:
             f = urllib.urlopen(location + query)
             s = f.read()
+
         except IOError, e:
             logger.warning("Error connecting to monitor: %s" % (e))
             return data
+
         sessionparser = SessionParser()
         sessionparser.parse(s)
 
@@ -35,6 +82,6 @@ class MonitorSession(Monitor):
         return data
 
 
-    # close monitor connection
     def close(self):
+        """Close monitor connection."""
         return
