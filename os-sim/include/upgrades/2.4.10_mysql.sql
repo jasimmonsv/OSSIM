c61e6d21e2f77d3c92c8556c32752f5c36b51f5a
Juan Manuel Albarracin <jmalbarracin@alienvault.com>
2010-12-09 11:35:06 +0100

    Fixed character enconding with new form checks

--- os-sim/include/upgrades/2.4.10_mysql.sql
+++ os-sim/include/upgrades/2.4.10_mysql.sql
@@ -3,6 +3,7 @@ SET AUTOCOMMIT=0;
 BEGIN;
 
 REPLACE INTO `inventory_search` (`type`, `subtype`, `match`, `list`, `query`, `ruleorder`) VALUES ('Vulnerabilities', 'Vuln Contains', 'text', '', 'SELECT DISTINCT INET_NTOA(hp.host_ip) as ip FROM host_plugin_sid hp, plugin_sid p WHERE hp.plugin_id = 3001 AND p.plugin_id = 3001 AND hp.plugin_sid = p.sid AND p.name %op% ? UNION SELECT DISTINCT INET_NTOA(s.host_ip) as ip FROM vuln_nessus_plugins p,host_plugin_sid s WHERE s.plugin_id=3001 and s.plugin_sid=p.id AND p.name %op% ?', 4);
+UPDATE custom_report_types SET inputs=REPLACE(inputs,";Source:source:select:OSS_ALPHA:EVENTSOURCE:",";Plugin Groups:plugin_groups:select:OSS_DIGIT.OSS_NULLABLE:PLUGINGROUPS:;Source:source:select:OSS_ALPHA:EVENTSOURCE:") WHERE file='SIEM/List.php' AND name!="List" AND inputs not like '%PLUGINGROUPS%';
 
 -- From now on, always add the date of the new releases to the .sql files
 use ossim;
