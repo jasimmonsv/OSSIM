d16d31fb32782ad0fec0b0e5131cbc002ab2d0a8
Pablo Vargas <pvargas@alienvault.com>
2011-01-11 00:08:07 +0100

    Added filter improvements

--- os-sim/www/sem/filter_range_and_sort.pl
+++ os-sim/www/sem/filter_range_and_sort.pl
@@ -149,10 +149,16 @@ foreach my $file (@files) {
 			# msandulescu filter improvement
 			#
 			my $pre_filter = $filter;
-			$pre_filter =~ s/^[^ ]*= AND //;    # remove broken entries like: data= AND data="string"; This should actually be fixed where $filer is set first, to apply both here and in the set_filter function.
-			$pre_filter =~ s/ .*//;		    # keep just the first filter expression
-			$pre_filter =~ s/=/\[^=]*=\[^=]*/;  # create the grep filter
-			$pre_cmd = ($file =~ /\.gz$/) ? "zcat \"$file\" |grep -l \"$pre_filter\"|" : "$order_by \"$file\" |grep -l \"$pre_filter\"|"; # -l stops on the first match
+            if ( $pre_filter !~ /^[^ ]*\!=]*/ ) {
+                $pre_filter =~ s/^[^ ]*= AND //; # remove broken entries like: data= AND data="string"; This should actually be fixed where $filer is set first, to apply both here and in the set_filter function.
+                $pre_filter =~ s/^[^ ]*\!=[^ ]*//;
+                $pre_filter =~ s/ .*//; # keep just the first filter expression
+                $pre_filter =~ s/=/\[^=]*=\[^=]*/; # create the grep filter
+                $pre_filter =~ s/\./\\./g;
+                $pre_cmd = ($file =~ /\.gz$/) ? "zcat \"$file\" |grep -l \"$pre_filter\"|" : "$order_by \"$file\" |grep -l \"$pre_filter\"|"; # -l stops on the first match
+            } else {
+                $pre_cmd = ($file =~ /\.gz$/) ? "zcat \"$file\"|" : "$order_by \"$file\" |";
+            }
 			open(F,$pre_cmd );
 			my $first_line = <F>;
 			close F;
