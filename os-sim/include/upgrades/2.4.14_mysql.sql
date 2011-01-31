10bee9c0ba3b75f8f32ec51224cdc3612b1f57f6
Pablo Vargas <pvargas@alienvault.com>
2011-01-31 09:48:24 +0100

    Changed menu orders, descriptions, and some news

--- os-sim/include/upgrades/2.4.14_mysql.sql
+++ os-sim/include/upgrades/2.4.14_mysql.sql
@@ -2,6 +2,48 @@ use ossim;
 SET AUTOCOMMIT=0;
 BEGIN;
 
+DELETE FROM `acl_templates_perms` WHERE `ac_perm_id` =6;
+DELETE FROM `acl_perm` WHERE `id` =6;
+DELETE FROM `acl_templates_perms` WHERE `ac_perm_id` =52;
+DELETE FROM `acl_perm` WHERE `id` =52;
+DELETE FROM `acl_templates_perms` WHERE `ac_perm_id` =45;
+DELETE FROM `acl_perm` WHERE `id` =45;
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.00' WHERE `acl_perm`.`id` =60;
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.01' WHERE `acl_perm`.`id` =70;
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.02' WHERE `acl_perm`.`id` =19;
+UPDATE `ossim`.`acl_perm` SET `ord` = '03.03' WHERE `acl_perm`.`id` =51;
+
+UPDATE `ossim`.`acl_perm` SET `description` = 'Top Frame' WHERE `acl_perm`.`id` =68;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Analysis -> Vulnerabilities -> View' WHERE `acl_perm`.`id` =49;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Reports -> Custom Reports',`ord` = '04.02'  WHERE `acl_perm`.`id` =65;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Assets -> Assets Discovery',`ord` = '05.06',name='MenuPolicy' WHERE `acl_perm`.`id` =42;
+UPDATE `ossim`.`acl_perm` SET `ord` = '08.06' WHERE `acl_perm`.`id` =36;
+UPDATE `ossim`.`acl_perm` SET `ord` = '08.07' WHERE `acl_perm`.`id` =17;
+UPDATE `ossim`.`acl_perm` SET `ord` = '08.08' WHERE `acl_perm`.`id` =57;
+UPDATE `ossim`.`acl_perm` SET `ord` = '08.09' WHERE `acl_perm`.`id` =41;
+UPDATE `ossim`.`acl_perm` SET `ord` = '08.10' WHERE `acl_perm`.`id` =44;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Configuration -> SIEM Components -> Sensors',`ord` = '08.04',name='MenuConfiguration' WHERE `acl_perm`.`id` =12;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Configuration -> SIEM Components -> Servers',`ord` = '08.05',name='MenuConfiguration' WHERE `acl_perm`.`id` =53;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Status -> Sensors',`ord` = '09.01',name='MenuStatus' WHERE `acl_perm`.`id` =29;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Status -> Sensors -> User Activity',`ord` = '09.02',name='MenuStatus' WHERE `acl_perm`.`id` =69;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Configuration -> Collection -> Data Sources' WHERE `acl_perm`.`id` =36;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Configuration -> Collection -> DS Groups' WHERE `acl_perm`.`id` =17;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Configuration -> Collection -> Downloads',`ord` = '08.08',name='MenuConfiguration' WHERE `acl_perm`.`id` =57;
+UPDATE `ossim`.`acl_perm` SET `description` = 'Configuration -> Backup',`ord` = '08.10',name='MenuConfiguration' WHERE `acl_perm`.`id` =44;
+
+INSERT IGNORE INTO `ossim`.`acl_perm` (`id` ,`type` ,`name` ,`value` ,`description` ,`granularity_sensor` ,`granularity_net` ,`enabled` ,`ord`) VALUES (72 , 'MENU', 'MenuEvents', 'EventsVulnerabilitiesScan', 'Analysis -> Vulnerabilities -> Scan/Import', '1', '1', '1', '03.08');
+INSERT IGNORE INTO `ossim`.`acl_perm` (`id` ,`type` ,`name` ,`value` ,`description` ,`granularity_sensor` ,`granularity_net` ,`enabled` ,`ord`) VALUES (73 , 'MENU', 'MenuEvents', 'EventsVulnerabilitiesDeleteScan', 'Analysis -> Vulnerabilities -> Delete Scan Report', '1', '1', '1', '03.09');
+INSERT IGNORE INTO `ossim`.`acl_perm` (`id` , `type` , `name` , `value` , `description` , `granularity_sensor` , `granularity_net` , `enabled` , `ord`) VALUES (74 , 'MENU', 'MenuReports', 'ReportsCreateCustom', 'Reports -> Custom Reports -> Create Custom Report', '0', '0', '1', '04.06');
+INSERT IGNORE INTO `ossim`.`acl_perm` (`id` , `type` , `name` , `value` , `description` , `granularity_sensor` , `granularity_net` , `enabled` , `ord`) VALUES (75 , 'MENU', 'MenuReports', 'ReportsScheduler', 'Reports -> Custom Reports -> Scheduler', '0', '0', '1', '04.07');
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.06' WHERE `acl_perm`.`id` =24;
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.07' WHERE `acl_perm`.`id` =23;
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.08' WHERE `acl_perm`.`id` =25;
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.09' WHERE `acl_perm`.`id` =40;
+UPDATE `ossim`.`acl_perm` SET `ord` = '02.10' WHERE `acl_perm`.`id` =55;
+INSERT IGNORE INTO `ossim`.`acl_perm` (`id`, `type`, `name`, `value`, `description`, `granularity_sensor`, `granularity_net`, `enabled`, `ord`) VALUES (76, 'MENU', 'MenuIncidents', 'IncidentsOpen', 'Incidents -> Tickets -> Open Tickets', '0', '0', '1', '02.04');
+INSERT IGNORE INTO `ossim`.`acl_perm` (`id`, `type`, `name`, `value`, `description`, `granularity_sensor`, `granularity_net`, `enabled`, `ord`) VALUES (77, 'MENU', 'MenuIncidents', 'IncidentsDelete', 'Incidents -> Tickets -> Delete', '0', '0', '1', '02.05');
+
+
 ALTER TABLE host_property_reference ADD  `description` VARCHAR( 128 ) NOT NULL;
 REPLACE INTO host_property_reference (`id`, `name`, `ord`, `description`) VALUES
 (1, 'software', 3, 'Software'),
