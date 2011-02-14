45817015cb4956b581f7b2797976579c1eabee0e
Pablo Vargas <pvargas@alienvault.com>
2011-02-14 12:26:34 +0100

    Changed Monitors to Situational Awareness in menu

--- os-sim/include/upgrades/2.4.18_mysql.sql
+++ os-sim/include/upgrades/2.4.18_mysql.sql
@@ -4,6 +4,10 @@ BEGIN;
 
 UPDATE IGNORE vuln_nessus_servers SET max_scans = '5';
 
+UPDATE `acl_perm` SET `description` = 'Situational Awareness -> Network -> Profiles' WHERE `acl_perm`.`id` =27;
+UPDATE `acl_perm` SET `description` = 'Situational Awareness -> Availability' WHERE `acl_perm`.`id` =28;
+UPDATE `acl_perm` SET `description` = 'Situational Awareness -> Network -> Traffic' WHERE `acl_perm`.`id` =66;
+
 -- WARNING! Keep this at the end of this file
 -- ATENCION! Keep this at the end of this file
 use ossim;
