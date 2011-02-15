ad9342d5963143eaba2dae680ba6e0f0e0c7f828
Pablo Vargas <pvargas@alienvault.com>
2011-02-15 10:07:55 +0100

    Changes in acl_perm table MenuStatus and ossim_acl.inc

--- os-sim/include/upgrades/2.4.18_mysql.sql
+++ os-sim/include/upgrades/2.4.18_mysql.sql
@@ -7,6 +7,8 @@ UPDATE IGNORE vuln_nessus_servers SET max_scans = '5';
 UPDATE `acl_perm` SET `description` = 'Situational Awareness -> Network -> Profiles' WHERE `acl_perm`.`id` =27;
 UPDATE `acl_perm` SET `description` = 'Situational Awareness -> Availability' WHERE `acl_perm`.`id` =28;
 UPDATE `acl_perm` SET `description` = 'Situational Awareness -> Network -> Traffic' WHERE `acl_perm`.`id` =66;
+UPDATE `acl_perm` SET `name` = 'MenuConfiguration', `description` = 'Configuration -> Collection -> Sensors' WHERE `acl_perm`.`id` =29;
+UPDATE `acl_perm` SET `name` = 'MenuConfiguration', `description` = 'Configuration -> Collection -> User Activity' WHERE `acl_perm`.`id` =69;
 
 DROP PROCEDURE IF EXISTS addcol;
 DELIMITER '//'
