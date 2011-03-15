ce81f2fc0c17bf021b201187727ef042aa31751f
Juan Manuel Albarracin <jmalbarracin@alienvault.com>
2011-03-15 13:00:39 +0100

    NF Solera Deepsee integration files

---
+++ os-sim/www/conf/solera.php
@@ -0,0 +1,110 @@
+<?php
+/*****************************************************************************
+*
+*    License:
+*
+*   Copyright (c) 2003-2006 ossim.net
+*   Copyright (c) 2007-2009 AlienVault
+*   All rights reserved.
+*
+*   This package is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; version 2 dated June, 1991.
+*   You may not use, modify or distribute this program under any other version
+*   of the GNU General Public License.
+*
+*   This package is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this package; if not, write to the Free Software
+*   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+*   MA  02110-1301  USA
+*
+*
+* On Debian GNU/Linux systems, the complete text of the GNU General
+* Public License can be found in `/usr/share/common-licenses/GPL-2'.
+*
+* Otherwise you can read it here: http://www.gnu.org/licenses/gpl-2.0.txt
+****************************************************************************/
+/**
+* Class and Function List:
+* Function list:
+* Classes list:
+*/
+require_once 'classes/Session.inc';
+require_once 'classes/Security.inc';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <link rel="stylesheet" href="../style/style.css"/>
+    <script src="../js/jquery-1.3.2.min.js" type="text/javascript" ></script>
+</head>
+<body>
+<?php
+$from = POST("from");
+$to = POST("to");
+$src_ip = POST("src_ip");
+$src_port = POST("src_port");
+$dst_ip = POST("dst_ip");
+$dst_port = POST("dst_port");
+$proto = POST("proto");
+ossim_valid($from, OSS_DATETIME, 'illegal:' . _("From date"));
+ossim_valid($to, OSS_DATETIME, 'illegal:' . _("To date"));
+ossim_valid($src_ip, OSS_IP_ADDR, 'illegal:' . _("Src IP"));
+ossim_valid($src_port, OSS_DIGIT, 'illegal:' . _("Src port"));
+ossim_valid($dst_ip, OSS_IP_ADDR, 'illegal:' . _("Dst IP"));
+ossim_valid($dst_port, OSS_DIGIT, 'illegal:' . _("Dst port"));
+ossim_valid($proto, "tcp|udp|icmp", 'illegal:' . _("Protocol"));
+if (ossim_error()) {
+    die(ossim_error());
+}
+if (POST("action")=="submit") {
+	require_once "ossim_conf.inc";
+	$conf = $GLOBALS["CONF"];
+	$host = $conf->get_conf("solera_host", FALSE);
+	$port = $conf->get_conf("solera_port", FALSE);
+	$user = $conf->get_conf("solera_user", FALSE);
+	$pass = $conf->get_conf("solera_pass", FALSE);
+	// Solera DeepSee sample url
+	// https://192.168.2.52/deepsee_reports?user=admin&password=Solera#pathString=/timespan/02.02.2011.21.09.11.02.02.2011.21.09.11/tcp_port/57676_and_80/ipv4_address/204.128.123.4_and_10.1.3.5/;reportIndex=0
+	$solera_url = "https://$host:$port/deepsee_reports?user=$user&password=$pass#pathString=/timespan/".$from.".".$to."/".$proto."_port/".$src_port."_and_".$dst_port."/ipv4_address/".$src_ip."_and_".$dst_ip."/;reportIndex=0";
+	?>
+	<script>
+		$(document).ready(function() { 
+			window.open('<?=$solera_url?>','solera_deepsee','toolbar=0,location=0,menubar=0,modal=yes'); 
+			parent.GB_hide();
+	 	});
+	</script>
+	<?
+} else {
+
+?>
+<center>
+<br>
+<form action="solera.php" method="post">
+<input type="hidden" name="action" value="submit">
+<table cellpadding="3">
+<th colspan="2">Solera DeepSee &trade; <?=_("Custom submit")?></th>
+<tr><td><?=_("Start time")?>:</td><td class="left"><input type="text" name="from" value="<?=$from?>"></td></tr>
+<tr><td><?=_("Stop time")?>:</td><td class="left"><input type="text" name="to" value="<?=$to?>"></td></tr>
+<tr><td><?=_("IP Protocol")?>:</td><td class="left"><select name="proto">
+	<option value="tcp"<? if ($proto=="tcp") echo " selected='selected'";?>>TCP
+	<option value="udp"<? if ($proto=="udp") echo " selected='selected'";?>>UDP
+	<option value="icmp"<? if ($proto=="icmp") echo " selected='selected'";?>>ICMP
+</select></td></tr>
+<tr><td><?=_("Source")?>:</td><td class="left"><input type="text" name="src_ip" value="<?=$src_ip?>">:<input type="text" name="src_port" size="4" value="<?=$src_port?>"></td></tr>
+<tr><td class="noborder"><?=_("Destination")?>:</td><td class="left noborder"><input type="text" name="dst_ip" value="<?=$dst_ip?>">:<input type="text" name="dst_port" size="4" value="<?=$dst_port?>"></td></tr>
+</table>
+<br><br>
+<input type="submit" class="button" value="<?=_("Submit")?>">
+<form>
+</center>
+<?
+}
+?>
+</body>
+</html>
