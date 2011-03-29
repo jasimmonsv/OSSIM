0323ddf0563b325d3bba0438b2f079b5d3563c02
Francisco Javier Rubio <fjrubio@alienvault.com>
2011-03-29 18:29:38 +0200

    Improvements in section Email templates

--- os-sim/www/conf/emailtemplate.php
+++ os-sim/www/conf/emailtemplate.php
@@ -42,22 +42,21 @@ require_once 'ossim_db.inc';
 require_once 'classes/Util.inc';
 
 Session::logcheck('MenuIncidents', 'ConfigurationEmailTemplate');
-$db = new ossim_db();
+$db   = new ossim_db();
 $conn = $db->connect();
-foreach(array(
-    'preview',
-    'subject_tpl',
-    'body_tpl',
-    'save'
-) as $var) {
+
+foreach(array('preview', 'subject_tpl','body_tpl', 'save') as $var)
+ {
     $$var = POST($var);
 }
 // User wants the default template
-if (GET('reset')) {
+if (GET('reset')) 
+{
     $save = true;
     $subject_tpl = $body_tpl = '';
 }
 // Save values in the "config" table
+
 if ($save) {
     Incident_ticket::save_email_template($subject_tpl, $body_tpl);
     header("Location: " . $_SERVER['SCRIPT_NAME']);
@@ -177,65 +176,86 @@ $labels = array(
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-  <title> <?php
-echo gettext("OSSIM Framework"); ?> </title>
-  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-  <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
-  <link rel="stylesheet" type="text/css" href="../style/style.css"/>
-<script language="JavaScript" type="text/javascript">
+	<title> <?php echo gettext("OSSIM Framework"); ?> </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+	<meta http-equiv="Pragma" content="no-cache"/>
+	<link rel="stylesheet" type="text/css" href="../style/style.css"/>
+	<script type="text/javascript" src="../js/jquery-1.3.2.min.js"></script>
+	<script type="text/javascript" src="../js/jquery-ui-1.7.custom.min.js"></script>
+	<script type="text/javascript" src="../js/jquery.simpletip.js"></script>
+	<script type="text/javascript">
 
-    function confirm_reset(text)
-    {
-        ret = confirm(text);
-        if (ret) {
-            document.location = '<?php echo $_SERVER['SCRIPT_NAME'] ?>?reset=1';
-        }
-        return ret
-    }
+		function confirm_reset(text)
+		{
+			ret = confirm(text);
+			if (ret) {
+				document.location = '<?php echo $_SERVER['SCRIPT_NAME'] ?>?reset=1';
+			}
+			return ret
+		}
 
-    // Precious code from Dokuwiki! (dokuwiki/lib/scripts/script.js)
-    function insertAtCarret(field,value)
-    {
-      //IE support
-      if (document.selection) {
-        field.focus();
-        if (opener == null) {
-          var sel = document.selection.createRange();
-        } else {
-          var sel = opener.document.selection.createRange();
-        }
-        sel.text = value;
-      //MOZILLA/NETSCAPE support
-      } else if (field.selectionStart || field.selectionStart == '0') {
-        var startPos  = field.selectionStart;
-        var endPos    = field.selectionEnd;
-        var scrollTop = field.scrollTop;
-        field.value = field.value.substring(0, startPos)
-                      + value
-                      + field.value.substring(endPos, field.value.length);
-    
-        field.focus();
-        var cPos=startPos+(value.length);
-        field.selectionStart=cPos;
-        field.selectionEnd=cPos;
-        field.scrollTop=scrollTop;
-      } else {
-        field.value += "\n"+value;
-      }
-      // reposition cursor if possible
-      if (field.createTextRange) field.caretPos = document.selection.createRange().duplicate();
-    }
+		// Precious code from Dokuwiki! (dokuwiki/lib/scripts/script.js)
+		function insertAtCarret(field,value)
+		{
+		  //IE support
+		  if (document.selection) {
+			field.focus();
+			if (opener == null) {
+			  var sel = document.selection.createRange();
+			} else {
+			  var sel = opener.document.selection.createRange();
+			}
+			sel.text = value;
+		  //MOZILLA/NETSCAPE support
+		  } else if (field.selectionStart || field.selectionStart == '0') {
+			var startPos  = field.selectionStart;
+			var endPos    = field.selectionEnd;
+			var scrollTop = field.scrollTop;
+			field.value = field.value.substring(0, startPos)
+						  + value
+						  + field.value.substring(endPos, field.value.length);
+		
+			field.focus();
+			var cPos=startPos+(value.length);
+			field.selectionStart=cPos;
+			field.selectionEnd=cPos;
+			field.scrollTop=scrollTop;
+		  } else {
+			field.value += "\n"+value;
+		  }
+		  // reposition cursor if possible
+		  if (field.createTextRange) field.caretPos = document.selection.createRange().duplicate();
+		}
 
-    // Interface to insertAtCarret()
-    function insertAtCursor(myField)
-    {
-      var tags = document.myform.tags;
-      var index = tags.selectedIndex;
-      var myValue = tags.options[index].text;
-      insertAtCarret(myField, myValue);
-    }
+		// Interface to insertAtCarret()
+		function insertAtCursor(myField)
+		{
+			var tags    = document.myform.tags;
+			var index   = tags.selectedIndex;
+			if (index >= 0 )
+			{
+				var myValue = tags.options[index].text;
+				insertAtCarret(myField, myValue);
+			}
+			else
+				alert('<?php echo _("You have to select a tag")?>');
+		}
+		
+		$(document).ready(function(){
+			$(".to_add").simpletip({
+					position: 'left',
+					baseClass: 'btooltip',
+					onBeforeShow: function() {
+						this.update('<?php echo _("Select a tag to add") ?>');
+					}
+			});
+		});
+		
 </script>
-  
+
+<style type='text/css'>
+	#help {margin-bottom: 10px;}
+</style>
 
 </head>
 <body>
@@ -243,92 +263,94 @@ echo gettext("OSSIM Framework"); ?> </title>
 <?php
 include ("../hmenu.php"); ?>
   
-<div id="help" class="help" width="70%">
-<i><?php echo _("Select a TAG to see its meaning") ?></i>
-</div>
-<br/>
-<form name="myform" method="POST">
-<table width="90%" border="0" align="center">
+<div id="help" class="ossim_info"><span style='font-style:italic'><?php echo _("Select a TAG to see its meaning") ?></span></div>
 
-<tr valign="top">
-    <td>
-    <?php echo _("Template Labels") ?><br/>
-    <select name="tags" size="21" onChange="javascript: show_help(this);">
-        <?php
-$i = 0;
-foreach($labels as $label => $data) {
-    $help_msgs[$i++] = addslashes($data['help']);
-?>
-        <option name="<?php echo $label
-?>"><?php echo $label
-?></option>
-        <?php
-} ?>
-    </select>
-    </td>
-    <td>
-        <table width="100%">
-            <tr>
-                <td>
-                    <input type="button" value="->" onClick="javascript: insertAtCursor(document.myform.subject_tpl);" class="lbutton" style="font-size:12px">
-                </td>
-                <th width="10%"><?php echo _("Subject") ?></th>
-                <td style="text-align: left;">
-                    <input type="text" name="subject_tpl" value="<?php echo $subject_tpl ?>" size="80" style="font-family: mono-space, mono;"/>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="button" value="->" onClick="javascript: insertAtCursor(document.myform.body_tpl);" class="lbutton" style="font-size:12px">
-                </td>
-                <th valign="top" width="10%"><?php
-echo gettext("Body"); ?></th>
-                <td style="text-align: left;">
-                    <textarea name="body_tpl" rows="25" cols="80" WRAP=HARD style="font-family: mono-space, mono;"><?php echo $body_tpl ?></textarea>
-                </td>
-            </tr>                
-        </table>
-</tr>
-</table>
-<script>
+<form name="myform" method="POST">
+
+<table width="90%" border="0" align="center">
+	<tr valign="top">
+		<td class='nobborder left'>
+			<div style='padding: 0px 0px 3px 0px'><?php echo _("Template Labels") ?></div>
+			<select name="tags" size="21" onChange="javascript: show_help(this);">
+				<?php
+				$i = 0;
+				foreach($labels as $label => $data) 
+				{
+					$help_msgs[$i++] = addslashes($data['help']);
+					?>
+					<option name="<?php echo $label?>"><?php echo $label?></option>
+					<?php
+				} ?>
+			</select>
+		</td>
+		
+		<td class='nobborder'>
+			<table width="100%">
+				<tr>
+					<td style="text-align: center;">
+						<!--<input type="button" value="->" onClick="javascript: insertAtCursor(document.myform.subject_tpl);" class="lbutton"/>-->
+						<a style='cursor:pointer;' class='to_add' onClick="javascript: insertAtCursor(document.myform.subject_tpl);"><img src='../pixmaps/play_blue.png' align='absmiddle'/></a>
+					</td>
+					<th width="10%"><?php echo _("Subject") ?></th>
+					<td class='left'>
+						<input type="text" name="subject_tpl" value="<?php echo $subject_tpl ?>" size="80" style="font-family: mono-space, mono;"/>
+					</td>
+				</tr>
+				
+				<tr>
+					<td class='nobborder' style="text-align: center !important;">
+						<a style='cursor:pointer;' class='to_add' onClick="javascript: insertAtCursor(document.myform.body_tpl);"><img src='../pixmaps/play_blue.png' align='absmiddle'/></a>
+					</td>
+					<th valign="top" width="10%"><?php echo gettext("Body"); ?></th>
+					<td style="text-align: left;" class='nobborder'>
+						<textarea name="body_tpl" rows="25" cols="80" wrap='hard' style="font-family: mono-space, mono;"><?php echo $body_tpl ?></textarea>
+					</td>
+				</tr>                
+			</table>
+		</tr>
+	</table>
+
+	<script type='text/javascript'>
     function show_help(select_el)
     {
         var selected = select_el.selectedIndex;
         var help = new Array;
-        <?php
-foreach($help_msgs as $key => $text) { ?>
-            help[<?php echo $key
-?>] = '<?php echo $text
-?>';
-        <?php
-} ?>
+		<?php
+		foreach($help_msgs as $key => $text) { 
+			?>
+			help[<?php echo $key?>] = '<?php echo $text?>';
+				<?php
+		} 
+		?>
         document.getElementById('help').innerHTML = '<b>'+help[selected]+'</b>';
         return false;
     }
 
-</script>
-<p align="center">
-    <input type="submit" name="preview" value="<?php echo _('Preview') ?>" class="button" style="font-size:12px">&nbsp;
-    <input type="button" name="reset" value="<?php echo _('Reset to Defaults') ?>" class="button" style="font-size:12px"
-           onClick="javscript: return confirm_reset('<?php echo addslashes(_("All changes will be lost. Continue anyway?")) ?>')" >
-    &nbsp;<input type="submit" name="save" value="<?php echo _('Save Template') ?>"  class="button" style="font-size:12px">
-</p>
+	</script>
+	
+	<p align="center">
+		<input type="submit" name="preview" value="<?php echo _('Preview') ?>" class="button"/>&nbsp;
+		<input type="button" name="reset" value="<?php echo _('Reset to Defaults') ?>" class="button"  onClick="javscript: return confirm_reset('<?php echo addslashes(_("All changes will be lost. Continue anyway?")) ?>')"/>
+		&nbsp;<input type="submit" name="save" value="<?php echo _('Save Template') ?>"  class="button" />
+	</p>
 </form>
+
 <?php
-if ($preview) {
+if ($preview) 
+{
     foreach($labels as $k => $data) {
         $values[$k] = $data['sample'];
     }
     $subject = Incident_ticket::build_email_template($subject_tpl, $values);
-    $body = Incident_ticket::build_email_template($body_tpl, $values);
+    $body    = Incident_ticket::build_email_template($body_tpl, $values);
 ?>
     <table align="center" width="80%">
     <tr>
-        <th valign="top" width="10%" style="text-align: right;"><?php echo _("Subject") ?>:</td>
+        <th valign="middle" width="10%" style="text-align: center;"><?php echo _("Subject") ?>:</td>
         <td valign="top" style="text-align: left;"><pre><?php echo $subject ?></pre></td>
     </tr>
     <tr>
-        <th valign="top" width="10%" style="text-align: right;"><?php echo _("Body") ?>:</td>
+        <th valign="middle" width="10%" style="text-align: center;"><?php echo _("Body") ?>:</td>
         <td valign="top" style="text-align: left; border-width: 0px;"><pre><?php echo Util::htmlentities($body, ENT_COMPAT, "UTF-8") ?></pre></td>
     </tr>
     </table>
