adb190f6cdbde50c33d7d8a08933b1fb4ef6c457
Pablo Vargas <pvargas@alienvault.com>
2011-04-07 18:08:15 +0200

    FIX #845: Do not show alarm summary in pagination

--- os-sim/www/control_panel/events_ajax.php
+++ os-sim/www/control_panel/events_ajax.php
@@ -148,7 +148,7 @@ if ($alarm_list = Alarm::get_events($conn, $backlog_id, $show_all, $event_id)) {
 	$count_events = 0;
     $count_alarms = 0;
     foreach($alarm_list as $alarm) {
-		if (!$alarm->get_alarm() && ($count_events < $from || $count_events >= $to)) {
+		if ($show_all > 1 && ($count_events < $from || $count_events >= $to)) {
 			$count_events++;
 			continue;
 		}
