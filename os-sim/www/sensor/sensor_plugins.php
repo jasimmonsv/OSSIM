0641ba814c7a45513015edb479ed9a8a48e14f9a
Francisco Javier Rubio <fjrubio@alienvault.com>
2011-05-09 17:08:53 +0200

    Fixed #1010 added improvement to load plugins

--- os-sim/www/sensor/sensor_plugins.php
+++ os-sim/www/sensor/sensor_plugins.php
@@ -35,73 +35,6 @@
 * Classes list:
 */
 require_once ('classes/Session.inc');
-Session::logcheck("MenuConfiguration", "MonitorsSensors");
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<head>
-<title> <?php
-echo gettext("OSSIM Framework"); ?> </title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
-<link rel="stylesheet" type="text/css" href="../style/style.css"/>
-<script type="text/javascript" src="../js/jquery-1.3.2.min.js"></script>
-<script type="text/javascript">
-function contenido(id) {
-	$("#"+id).toggle();
-	id_icono = id.substr (4);
-	if ($("#icono"+id_icono).attr('src') == "../pixmaps/server--plus.png") { $("#icono"+id_icono).attr('src','../pixmaps/server--minus.png');  }
-	else { $("#icono"+id_icono).attr('src','../pixmaps/server--plus.png'); }
-}
-function load_lead(pid) {
-    $.ajax({
-        type: "GET",
-        url: "get_sensor_leads.php?pid="+pid,
-        data: "",
-        success: function(msg) {
-              $('#plugin_'+pid).html(msg);
-              $('#plugin_'+pid).show();
-              $('#selector_'+pid).show();
-              mark(pid);
-        }
-    });
-}
-function mark(id) {
-	var y = $('#yellow_'+id).val()*3600;
-	var r = $('#red_'+id).val()*3600; // need seconds
-	var now = new Date; // Generic JS date object
-	var unixtime_ms = now.getTime(); // Returns milliseconds since the epoch
-	var unixtime = parseInt(unixtime_ms / 1000);
-	$('#plugin_'+id+' .trc').each(function(){
-		var eventdate = parseInt($(this).attr('txt'));
-		var img = "";
-		var bgcolor = "";
-		if (unixtime - eventdate >= r) {
-			img = "../pixmaps/flag_red.png";
-			bgcolor = "#FFDFDF";
-		} else if (unixtime - eventdate >= y) {
-			img = "../pixmaps/flag_yellow.png";
-			bgcolor = "#FFFBCF";
-		} else {
-			img = "../pixmaps/flag_green.png";
-			bgcolor = "#CFFFD1";
-		}
-		$(this).css("background-color",bgcolor);
-		$('td img',this).attr("src",img);
-	});
-}
-</script>
-<? include ("../host_report_menu.php") ?>
-</head>
-<body>                             
-<?php
-include ("../hmenu.php");
-
-// Sensors perm check
-if (!Session::menu_perms("MenuConfiguration", "PolicySensors")) {
-	die("<br>"._("You need permissions of section '")."<b>"._("Configuration -> SIEM Components -> Sensors")."</b>"._("' to see this page. Contact with the administrator."));
-}
-
 require_once 'ossim_conf.inc';
 require_once 'ossim_db.inc';
 require_once 'classes/Sensor.inc';
@@ -143,10 +76,119 @@ if (!$sensor_list && empty($ip_get)) echo "<p> " . gettext("There aren't any sen
 
 $ossim_conf = $GLOBALS["CONF"];
 $use_munin = $ossim_conf->get_conf("use_munin");
-$capa = 0;
+
+Session::logcheck("MenuConfiguration", "MonitorsSensors");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title> <?php
+echo gettext("OSSIM Framework"); ?> </title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<link rel="stylesheet" type="text/css" href="../style/style.css"/>
+<script type="text/javascript" src="../js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript">
+function contenido(id) {
+	$("#"+id).toggle();
+	id_icono = id.substr (4);
+
+	if ($("#icono"+id_icono).attr('src') == "../pixmaps/server--plus.png") {
+        $("#icono"+id_icono).attr('src','../pixmaps/server--minus.png');
+        var loading = '<img width="16" align="absmiddle" src="../vulnmeter/images/loading.gif">';
+        $("#"+id).html(loading+' <?php echo _("Loading data..."); ?>');
+        $("#"+id).css({ padding: "5px 0px 5px 0px" }); 
+        var ip = id.substr (4);
+        ip = ip.replace(/_/g, ".");
+        $.ajax({
+                type: "GET",
+                url: "get_sensor_info.php",
+                data: { sensor_ip: ip },
+                success: function(msg) {
+                    $("#"+id).css({ padding: "0px 0px 0px 0px" });
+                    $('#'+id).html(msg);
+                    
+                }
+        });
+    }
+	else {
+        $("#icono"+id_icono).attr('src','../pixmaps/server--plus.png');
+    }
+}
+function load_lead(pid) {
+    $.ajax({
+        type: "GET",
+        url: "get_sensor_leads.php?pid="+pid,
+        data: "",
+        success: function(msg) {
+              $('#plugin_'+pid).html(msg);
+              $('#plugin_'+pid).show();
+              $('#selector_'+pid).show();
+              mark(pid);
+        }
+    });
+}
+function mark(id) {
+	var y = $('#yellow_'+id).val()*3600;
+	var r = $('#red_'+id).val()*3600; // need seconds
+	var now = new Date; // Generic JS date object
+	var unixtime_ms = now.getTime(); // Returns milliseconds since the epoch
+	var unixtime = parseInt(unixtime_ms / 1000);
+	$('#plugin_'+id+' .trc').each(function(){
+		var eventdate = parseInt($(this).attr('txt'));
+		var img = "";
+		var bgcolor = "";
+		if (unixtime - eventdate >= r) {
+			img = "../pixmaps/flag_red.png";
+			bgcolor = "#FFDFDF";
+		} else if (unixtime - eventdate >= y) {
+			img = "../pixmaps/flag_yellow.png";
+			bgcolor = "#FFFBCF";
+		} else {
+			img = "../pixmaps/flag_green.png";
+			bgcolor = "#CFFFD1";
+		}
+		$(this).css("background-color",bgcolor);
+		$('td img',this).attr("src",img);
+	});
+}
+$(document).ready(function() {
+    <?php
+    if( $ip_get!="" && Session::sensorAllowed($ip_get) ) {?>
+        var loading = '<img width="16" align="absmiddle" src="../vulnmeter/images/loading.gif">';
+        var id = '<?php echo $ip_get;?>'; 
+        id = id.replace(/\./g, "_");
+        id = 'capa'+id;
+        $("#icono"+id).attr('src','../pixmaps/server--minus.png');
+        $("#"+id).html(loading+' <?php echo _("Loading data..."); ?>');
+        $("#"+id).css({ padding: "5px 0px 5px 0px" }); 
+        $.ajax({
+                type: "GET",
+                url: "get_sensor_info.php",
+                data: { sensor_ip: '<?php echo $ip_get; ?>' },
+                success: function(msg) {
+                    $("#"+id).css({ padding: "0px 0px 0px 0px" });
+                    $('#'+id).html(msg);
+                    $('#'+id).show();
+                }
+        });
+    <?php
+    }
+?>
+});
+</script>
+<? include ("../host_report_menu.php") ?>
+</head>
+<body>                             
+<?php
+include ("../hmenu.php");
+
+// Sensors perm check
+if (!Session::menu_perms("MenuConfiguration", "PolicySensors")) {
+	die("<br>"._("You need permissions of section '")."<b>"._("Configuration -> SIEM Components -> Sensors")."</b>"._("' to see this page. Contact with the administrator."));
+}
 
 foreach($sensor_list as $sensor) {
-
     $ip = $sensor["sensor"];
     unset($list_no_active[$ip]); // borramos de la lista de no activos los que están activos
     if (isset($db_sensor_rel[$ip])) $name = $db_sensor_rel[$ip];
@@ -198,7 +240,7 @@ foreach($sensor_list as $sensor) {
     }
 
     /* get plugin list for each sensor */
-    $sensor_plugins_list = server_get_sensor_plugins();
+    $sensor_plugins_list = server_get_sensor_plugins($ip);
     /*
     *  show sensor ip (and sensor name if available)
     *  at the top of the table
@@ -221,16 +263,16 @@ foreach($sensor_list as $sensor) {
             }
         }
     }
-    $id_estado = "icono" . $capa;
+    $id_estado = "icono" . str_replace(".","_",$ip);
     
     echo "<table class=\"noborder\" border=0 cellpadding=0 cellspacing=0 width=\"100%\" align=\"center\">";
     echo "<tr>";
-    echo "<td class=\"noborder\"><a href=\"\" onclick=\"contenido('capa$capa');return false;\"><img id='$id_estado' align=\"bottom\" src=".(($ip_get==$ip) ? "\"../pixmaps/server--minus.png\"" : "\"../pixmaps/server--plus.png\"")." border=\"0\"></a></td>";
+    echo "<td class=\"noborder\"><a href=\"\" onclick=\"contenido('capa".str_replace(".","_",$ip)."');return false;\"><img id='$id_estado' align=\"bottom\" src=".(($ip_get==$ip) ? "\"../pixmaps/server--minus.png\"" : "\"../pixmaps/server--plus.png\"")." border=\"0\"></a></td>";
     echo "<td class=\"noborder\" style=\"text-align: left;padding-left:5px;\" height=\"25\" bgcolor=\"#DCDCDC\" nowrap>";
     echo "<table class=\"noborder\" border=0 cellpadding=0 cellspacing=0 style=\"background-color:transparent;\" nowrap>";
     echo "<tr><td class=\"noborder\" style=\"padding-right:2px;\">";
     echo "</td><td class=\"noborder\" style=\"text-align: left;padding-right:4px;\">";
-    echo " <a href='' onclick=\"contenido('capa$capa');return false;\" class='HostReportMenu' id='$ip;".((isset($name)) ? $name : $ip)."'>$ip</a>";
+    echo " <a href='' onclick=\"contenido('capa".str_replace(".","_",$ip)."');return false;\" class='HostReportMenu' id='$ip;".((isset($name)) ? $name : $ip)."'>$ip</a>";
     if (isset($name)) echo " [ $name ] ";
     /*
     * Show munin link for every sensor
@@ -285,150 +327,12 @@ foreach($sensor_list as $sensor) {
     }
 ?>
 <tr><td class="noborder"></td>
-<td class="noborder">
-<div id="<?php echo "capa" . $capa ?>" style="display:<?=($ip_get==$ip) ? "block" : "none"?>">
-  <table class="noborder" width="100%" height="100%"><tr height="100%">
-  <td class="nobborder" width="36" height="100%">
-	  <table border=0 cellpadding=0 cellspacing=0 width="36" height="100%" class="noborder">
-	  <tr><td class="nobborder" height="29"><img src="../pixmaps/bktop.gif" border=0></td></tr>
-	  <tr><td class="nobborder" style="background:url(../pixmaps/bkbg.gif) repeat-y">&nbsp;</td></tr>
-	  <tr><td class="nobborder" height="51"><img src="../pixmaps/bkcenter.gif" border=0></td></tr>
-	  <tr><td class="nobborder" style="background:url(../pixmaps/bkbg.gif) repeat-y">&nbsp;</td></tr>
-	  <tr><td class="nobborder" height="29"><img src="../pixmaps/bkdown.gif" border=0></td></tr>
-	  </table>
-  </td><td class="nobborder" style="background:#E0EFC2;padding:5px">
-  <table align="left" width="100%">
-    <tr>
-      <th></th>
-      <th> <?php
-    echo gettext("Plugin"); ?> </th>
-      <th> <?php
-    echo gettext("Process Status"); ?> </th>
-      <th> <?php
-    echo gettext("Action"); ?> </th>
-      <th> <?php
-    echo gettext("Plugin status"); ?> </th>
-      <th> <?php
-    echo gettext("Action"); ?> </th>
-      <th> <?php
-    echo gettext("Last SIEM Event"); ?> </th>
-    </tr>
-<?php
-    if ($sensor_plugins_list) {	
-        foreach($sensor_plugins_list as $sensor_plugin) {
-            if ($sensor_plugin["sensor"] == $ip) {
-                $id = $sensor_plugin["plugin_id"];
-                $state = $sensor_plugin["state"];
-                $enabled = $sensor_plugin["enabled"];
-                if ($plugin_list = Plugin::get_list($conn, "WHERE id = $id")) {
-                    $plugin_name = $plugin_list[0]->get_name();
-                } else {
-                    $plugin_name = $id;
-                }
-                $event = Plugin::get_latest_SIM_Event($conn_snort,$id,$plugin_name);
-?>
-    <tr>
-      <td width="16"><a href="javascript:;" onclick="load_lead('<?=$id?>')"><img src="../pixmaps/plus-small.png" border="0" align="absmiddle"></a></td>
-      <td><?php
-                echo $plugin_name ?></td>
-<?php
-                if ($state == 'start') {
-?>
-      <td><font color="GREEN"><b> <?php
-                    echo gettext("UP"); ?> </b></font></td>
-      <td><a href="<?php
-                    echo $_SERVER["SCRIPT_NAME"] . "?sensor=$ip&ip=$ip&cmd=stop&id=$id" ?>">
-	    <?php
-                    echo gettext("stop"); ?> </a></td>
-<?php
-                } elseif ($state == 'stop') {
-?>
-      <td><font color="RED"><b> <?php
-                    echo gettext("DOWN"); ?> </b></font></td>
-      <td><a href="<?php
-                    echo $_SERVER["SCRIPT_NAME"] . "?sensor=$ip&ip=$ip&cmd=start&id=$id" ?>">
-	    <?php
-                    echo gettext("start"); ?> </a></td>
-      
-<?php
-                } else {
-                    echo "
-                          <td>"._("Unknown")."</td>
-                          <td>-</td>
-                        ";
-                }
-                if ($enabled == 'true') {
-?>
-      <td><font color="GREEN"><b> <?php
-                    echo gettext("ENABLED"); ?> </b></font></td>
-      <td><a href="<?php
-                    echo $_SERVER["SCRIPT_NAME"] . "?sensor=$ip&ip=$ip&cmd=disable&id=$id" ?>">
-	    <?php
-                    echo gettext("disable"); ?> </a></td>
-<?php
-                } else {
-?>
-      <td><font color="RED"><b> <?php
-                    echo gettext("DISABLED"); ?> </b></font></td>
-      <td><a href="<?php
-                    echo $_SERVER["SCRIPT_NAME"] . "?sensor=$ip&ip=$ip&cmd=enable&id=$id" ?>">
-	    <?php
-                    echo gettext("enable"); ?> </a></td>
-<?php
-                }
-?>
-      <td>
-			<table class="noborder"><tr>
-			<td class="small nobborder" nowrap><i><?=$event["timestamp"]?></i>&nbsp;</td>
-			<td class="small nobborder"><a href="<?=$acid_main_link."&plugin=".urlencode($event["plugin_id"])?>"><b><?=$event["sig_name"]?></b></a></td>
-			</tr></table>
-	  </td>
-    </tr>
-    <tr>
-    	<td colspan="2" id="selector_<?=$id?>" style="display:none;padding-left:10px;border-bottom:none">
-    		<form style="margin:0px"><table class="noborder center">
-    		<tr>
-    			<td class="noborder"><img src="../pixmaps/flag_yellow.png" border="0"></td>
-    			<td class="noborder"><input type="text" size="4" id="yellow_<?=$id?>" value="12"> <?=_("hours")?></td>
-    		</tr>
-    		<tr>
-    			<td class="noborder"><img src="../pixmaps/flag_red.png" border="0"></td>
-    			<td class="noborder"><input type="text" size="4" id="red_<?=$id?>" value="48"> <?=_("hours")?></td>
-    		</tr>
-    		<tr>
-    			<td colspan="2" class="noborder" align="center"><input type="button" class="button" onclick="mark('<?=$id?>')" value="<?=_("Mark")?>"></td>
-    		</tr>
-    		</table></form>
-    	</td>    
-    	<td colspan="5" id="plugin_<?=$id?>" style="display:none;padding-left:0px;border-bottom:none"></td>
-    </tr>
-<?php
-            } // if
-            
-        } // foreach
-        
-?>
-    <tr>
-      <td colspan="7">
-        <a href="<?php
-        echo $_SERVER["SCRIPT_NAME"] . "?sensor=$ip" ?>"> <?=_("Refresh")?> </a>
-      </td>
-    </tr>
-<?php
-    } // if
-    
-?>
- <!--        </table>
-     </td>
-    </tr>-->
-  </table>
-  </td></tr></table>
-</div>
+<td class="noborder" valign="top">
+    <div id="<?php echo "capa" . str_replace(".","_",$ip); ?>" style="diplay:none">
+    </div>
 </td></tr>
 
-
 <?php
-    $capa++;
 }
 foreach($list_no_active as $key => $value) {
     echo "<tr><td class=\"noborder\"><img align=\"bottom\" src=\"../pixmaps/server.png\" border=\"0\"></td>";
