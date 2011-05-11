0a30b2dc1ea435dc600734c957f5d5f105c9bd86
Pablo Vargas <pvargas@alienvault.com>
2011-05-11 15:45:41 +0200

    Changes in custom_report_types table Logger rows

--- os-sim/include/upgrades/2.4.28_mysql.sql
+++ os-sim/include/upgrades/2.4.28_mysql.sql
@@ -2,6 +2,14 @@ use ossim;
 SET AUTOCOMMIT=0;
 BEGIN;
 
+REPLACE INTO `custom_report_types` (`id`, `name`, `type`, `file`, `inputs`, `sql`, `dr`) VALUES
+(140, 'Top Attacker Host', 'Logger', 'Logger/AttackerHosts.php', 'Top Attacker Host:top:text:OSS_DIGIT:10:50;Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1),
+(141, 'Top Attacked Host', 'Logger', 'Logger/AttackedHosts.php', 'Top Attacked Host:top:text:OSS_DIGIT:10:50;Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1),
+(142, 'Top Used Ports', 'Logger', 'Logger/UsedPorts.php', 'Top Used Ports:top:text:OSS_DIGIT:10:50;Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1),
+(143, 'Data Sources', 'Logger', 'Logger/CollectionSources.php', 'Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1),
+(144, 'Events Trend', 'Logger', 'Logger/EventsTrend.php', 'Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1),
+(145, 'Top Events', 'Logger', 'Logger/List.php', 'Top Logger Events List:top:text:OSS_DIGIT:25:250;Product Type:sourcetype:select:OSS_ALPHA.OSS_SLASH.OSS_SPACE.OSS_NULLABLE:SOURCETYPE:;Event Category:category:select:OSS_DIGIT.OSS_NULLABLE:CATEGORY:;Event SubCategory:subcategory:select:OSS_DIGIT.OSS_NULLABLE:SUBCATEGORY:;Plugin Groups:plugin_groups:select:OSS_DIGIT.OSS_NULLABLE:PLUGINGROUPS:;Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1);
+
 -- ATENCION! Keep this at the end of this file
 use ossim;
 REPLACE INTO config (conf, value) VALUES ('last_update', '2011-05-06');
