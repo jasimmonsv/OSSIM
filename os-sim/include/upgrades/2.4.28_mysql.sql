33933f66db4e56c8ab23967f15511e4fb98b52b0
Pablo Vargas <pvargas@alienvault.com>
2011-05-16 16:42:02 +0200

    Added Attacks by Country subreport

--- os-sim/include/upgrades/2.4.28_mysql.sql
+++ os-sim/include/upgrades/2.4.28_mysql.sql
@@ -10,6 +10,9 @@ REPLACE INTO `custom_report_types` (`id`, `name`, `type`, `file`, `inputs`, `sql
 (144, 'Events Trend', 'Logger', 'Logger/EventsTrend.php', 'Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1),
 (145, 'Top Events', 'Logger', 'Logger/List.php', 'Top Logger Events List:top:text:OSS_DIGIT:25:250;Product Type:sourcetype:select:OSS_ALPHA.OSS_SLASH.OSS_SPACE.OSS_NULLABLE:SOURCETYPE:;Event Category:category:select:OSS_DIGIT.OSS_NULLABLE:CATEGORY:;Event SubCategory:subcategory:select:OSS_DIGIT.OSS_NULLABLE:SUBCATEGORY:;Plugin Groups:plugin_groups:select:OSS_DIGIT.OSS_NULLABLE:PLUGINGROUPS:;Source:source:select:OSS_ALPHA:EVENTSOURCELOGGER:', '', 1);
 
+REPLACE INTO `custom_report_types` (`id`, `name`, `type`, `file`, `inputs`, `sql`, `dr`) VALUES
+(137, 'Top Attacks by Country', 'SIEM Events', 'SIEM/AttacksCountry.php', 'Top Attacked Host:top:text:OSS_DIGIT:10:50;Top Attacker Countries:num_countries:text:OSS_DIGIT:3:20;Product Type:sourcetype:select:OSS_ALPHA.OSS_SLASH.OSS_SPACE.OSS_NULLABLE:SOURCETYPE:;Event Category:category:select:OSS_DIGIT.OSS_NULLABLE:CATEGORY:;Event SubCategory:subcategory:select:OSS_DIGIT.OSS_NULLABLE:SUBCATEGORY:;Plugin Groups:plugin_groups:select:OSS_DIGIT.OSS_NULLABLE:PLUGINGROUPS:', '', 30);
+
 -- ATENCION! Keep this at the end of this file
 use ossim;
 REPLACE INTO config (conf, value) VALUES ('last_update', '2011-05-06');
