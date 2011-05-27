deb402994d405fe95f7c638e957ce2502cb5153a
Pablo Vargas <pvargas@alienvault.com>
2011-05-27 13:48:01 +0200

    NF def_asset added to config table

--- os-sim/include/upgrades/2.4.29_mysql.sql
+++ os-sim/include/upgrades/2.4.29_mysql.sql
@@ -2,6 +2,8 @@ use ossim;
 SET AUTOCOMMIT=0;
 BEGIN;
 
+REPLACE INTO config (conf, value) VALUES ('def_asset', '2');
+
 -- ATENCION! Keep this at the end of this file
 use ossim;
 REPLACE INTO config (conf, value) VALUES ('last_update', '2011-05-20');
