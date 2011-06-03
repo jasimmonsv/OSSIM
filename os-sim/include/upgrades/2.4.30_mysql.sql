5a3a6c57d2f6dc51455022aaf57ccb047e6ad007
Pablo Vargas <pvargas@alienvault.com>
2011-06-03 12:12:18 +0200

    Some changes in custom_report_types table

--- os-sim/include/upgrades/2.4.30_mysql.sql
+++ os-sim/include/upgrades/2.4.30_mysql.sql
@@ -3,6 +3,8 @@ SET AUTOCOMMIT=0;
 BEGIN;
 
 UPDATE custom_report_types SET inputs=CONCAT(inputs,";Group by:groupby:select:OSS_ALPHA.OSS_SCORE.OSS_PUNC.OSS_NULLABLE:TRENDGROUPBY:") WHERE file='Logger/EventsTrend.php' AND inputs not like '%TRENDGROUPBY%';
+UPDATE custom_report_types SET inputs=CONCAT(inputs,";Source:source:select:OSS_ALPHA:EVENTSOURCE:"),type='SIEM/Logger Events' WHERE file='SIEM/TopEvents.php' AND inputs not like '%EVENTSOURCE%';
+UPDATE custom_report_types SET file="Logger/TopEvents.php" WHERE name='Top Events' AND type='Logger';
 
 -- ATENCION! Keep this at the end of this file
 use ossim;
